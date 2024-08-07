47c873b9c3dc2dfd9999159af393dd200c71f1b2
Alina Bach <al.ba9424@gmail.com>
2024-08-07 16:03:42 +0200

    Added search bar functionality (untested), still missing json data, deleted input action from index.html

--- script.js
+++ script.js
@@ -3,17 +3,26 @@ document.querySelector("form").onsubmit = function() {
   let suche = document.querySelector("input").value;
   if (suche.length < 3) {
     alert("zu kurzer Suchbegriff");
-  } else {
-    alert("Suche nach: " + suche);
   }
-  return false;
 }
 
 const searchInput = document.querySelector("[data-search]");
 
 searchInput.addEventListener("input", (event) => {
   const value = event.target.value
-  console.log(value)
+  console.log(value);
 })
 
 fetch("https://jsonplaceholder.typicode.com/posts")
+.then(response => {
+  return result.json();
+})
+.then(data => {
+  data.forEach(section => {
+    const markup = '<li>${section.title}</li>';
+    document.querySelector('ul').insertAdjacentHTML('beforeend', markup);
+  });
+})
+.catch(error => {
+  console.log(error)
+})
