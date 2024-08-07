3f9f0e83b91a42ff40bc21d2a34a520fb056dc4f
Alina Bach <al.ba9424@gmail.com>
2024-08-07 14:34:20 +0200

    edited script.js, trying to make search bar return results

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-const searchInput = document.querySelector("[data-search]")
+document.querySelector("form").onsubmit = function() {
 
-searchInput.addEventListener("input", e => {
-  const value = e.target.value
+  let suche = document.querySelector("input").value;
+  if (suche.length < 3) {
+    alert("zu kurzer Suchbegriff");
+  } else {
+    alert("Suche nach: " + suche);
+  }
+  return false;
+}
+
+const searchInput = document.querySelector("[data-search]");
+
+searchInput.addEventListener("input", (event) => {
+  const value = event.target.value
   console.log(value)
 })
+
+fetch("https://jsonplaceholder.typicode.com/posts")
